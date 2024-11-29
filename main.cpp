24ba13547d2ec8748c7151bd12ffee8fc3874b08
Abdurrahman Alyajouri <70238647+UsualSpace@users.noreply.github.com>
2024-11-28 20:44:48 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,37 @@
+/*
+ * Filename: main.cpp
+ * Programmer: Abdurrahman Alyajouri
+ * Date: 11/25/2025
+ * Purpose: The purpose of this file is to test out basic regex operations
+ *          implemented in other files.
+ */
+
+#include "regex.h"
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::getline;
+
+int main() {
+    
+    //Pattern that matches the set of strings that begin with either "Hello"
+    //or "Hi", then a space and then "World" followed by 0 or more instances
+    //of the character 's'. Ex: "Hello Worldssssss".
+    Regex re("((Hello)|(Hi)) Worlds*");
+    
+    string s;
+    
+    do {
+        cout << "Enter a string that matches \"" + re.GetPattern() + "\" (Enter \"quit\" to quit): ";
+        getline(cin, s);
+        if(re.Match(s)) cout << '"' + s + "\" matches!\n";
+        else cout << '"' + s + "\" DOES NOT MATCH!!!11!\n";
+    } while(cin.good() && s != "quit");
+    
+    return 0;
+}
+
